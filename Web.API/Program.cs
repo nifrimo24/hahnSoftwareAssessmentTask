e37729f9c1e1d6309debda4709db31209d2d4a43
Nicolás Morillo <emorillo@confiamed.com>
2024-12-14 23:45:30 -0500

    remove useless endpoint

--- Web.API/Program.cs
+++ Web.API/Program.cs
@@ -25,25 +25,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
 #region CompanyEndpoints
 
 app.MapPost("/companies", async (ISender mediator, [FromBody] CreateCompanyCommand command) =>
